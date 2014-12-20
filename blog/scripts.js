de20b0e34c879689d5ab0ae48f05b1e11dba00fa
Denise Ichinco <ichinco@gmail.com>
2014-12-20 12:12:11 -0500

    reformat to work with github?

--- blog/scripts.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $.each($('.render-math'), function(i, elt) {
-    	try {
-        	katex.render($(elt).text(), elt);
-	} catch (err) {
-		console.log(err, $(elt).text());
-	}
-    });
-});
